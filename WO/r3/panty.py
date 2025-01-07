ae1a0be747795b933c071b675919b3a61348ba11
wezbest <110793713+wezbest@users.noreply.github.com>
2025-01-07 17:43:34 +0000

    🫦

--- WO/r3/panty.py
+++ WO/r3/panty.py
@@ -1,6 +1,4 @@
-from rich.console import Console
-from rich.markdown import Markdown
-import subprocess
+from src.fun import m1, m2
 from rich.traceback import install
 from rich import print as rprint
 
@@ -8,45 +6,8 @@ install(show_locals=True)
 
 
 def main():
-    m1()
-
-
-# Rendering Markup Function
-
-
-def m1():
-    subprocess.run(["clear"])
-    MARKDOWN = """
-# This is an h1
-
-Rich can do a pretty *decent* job of rendering markdown.
-
-1. This is a list item
-2. This is another list item
-
-# To Run the server - Server will staert with this command
-
-```py 
-uv run python manage.py runserver
-```
-
-# Now the server will be started with the command 
-
-"""
-
-    console = Console()
-    md = Markdown(MARKDOWN)
-    console.print(md)
-
-    # Run command
-    p = subprocess.Popen(["uv", "run", "python", "panty1/manage.py", "runserver"])
-    rprint("[magenta]Server started... press ctrl+c to stop[/magenta]")
-    try:
-        p.wait()
-    except KeyboardInterrupt:
-        p.terminate()
-        rprint("""[red]
-Server Stopped ..Fuck all nite ....[/red]""")
+    m1()  # Markup Funtion
+    m2()  # Server Run Function
 
 
 if __name__ == "__main__":
--- WO/r3/src/fun.py
+++ WO/r3/src/fun.py
@@ -1,5 +1,43 @@
-# These functions to be called in main 
+# These functions to be called in main
+from rich.console import Console
+from rich.markdown import Markdown
+import subprocess
+from rich import print as rprint
+
+
+def m1():
+    subprocess.run(["clear"])
+    MARKDOWN = """
+# This is an h1
+
+Rich can do a pretty *decent* job of rendering markdown.
+
+1. This is a list item
+2. This is another list item
+
+# To Run the server - Server will staert with this command
+
+```py 
+uv run python manage.py runserver
+```
+
+# Now the server will be started with the command 
+
+"""
+
+    console = Console()
+    md = Markdown(MARKDOWN)
+    console.print(md)
+
 
 # Function to run the django server
-def m1():
-    
+def m2():
+    # Run command
+    p = subprocess.Popen(["uv", "run", "python", "panty1/manage.py", "runserver"])
+    rprint("[magenta]Server started... press ctrl+c to stop[/magenta]")
+    try:
+        p.wait()
+    except KeyboardInterrupt:
+        p.terminate()
+        rprint("""[red]
+Server Stopped ..Fuck all nite ....[/red]""")
