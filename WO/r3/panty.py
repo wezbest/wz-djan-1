0f97066d14cba1158d7966ce4746898dc7f2b98f
wezbest <110793713+wezbest@users.noreply.github.com>
2025-01-07 15:13:59 +0000

    🫦

--- WO/r3/panty.py
+++ WO/r3/panty.py
@@ -1,6 +1,9 @@
 from rich.console import Console
 from rich.markdown import Markdown
 import subprocess
+from rich.traceback import install
+
+install(show_locals=True)
 
 
 def main():
@@ -20,17 +23,22 @@ Rich can do a pretty *decent* job of rendering markdown.
 1. This is a list item
 2. This is another list item
 
-# To Run the server 
+# To Run the server - Server will staert with this command
 
 ```py 
 uv run python manage.py runserver
 ```
+
+# Now the server will be started with the command 
 
 """
 
     console = Console()
     md = Markdown(MARKDOWN)
     console.print(md)
+
+    # Run command
+    subprocess.run(["uv run python manage.py runserver"])
 
 
 if __name__ == "__main__":
