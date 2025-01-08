c652a80cdeb410e2360a1a4ce079cca224a787c9
wezbest <110793713+wezbest@users.noreply.github.com>
2025-01-08 12:45:19 +0000

    🫦

--- WO/proj1/worldtour/asia/views.py
+++ WO/proj1/worldtour/asia/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
-# Create your views here.
-def index(request):
-    page = """
+# Variable for pages
+indexpage = """
+<head>
+   <style>
+     body {
+      margin: 0;
+      height: 100vh;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    }
+  </style>
+</head>
 <link
   rel="stylesheet"
   href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
 >
 <h1>Asia AntySnyfrs</h1>
 <h2>Asia ArmpitandAssSmells</h2>
+<p>Ass and pussy smells</p>
 <button>Asia StinkSmells</button>
 """
-    return HttpResponse(page)
+
+
+# Create your views here.
+def index(request):
+    return HttpResponse(indexpage)
