0b643542e98cefcdc84f20c707411059473ceeba
wezbest <110793713+wezbest@users.noreply.github.com>
2025-01-08 12:41:57 +0000

    🫦

--- WO/proj1/worldtour/asia/views.py
+++ WO/proj1/worldtour/asia/views.py
@@ -5,6 +5,10 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     page = """
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+>
 <h1>Asia AntySnyfrs</h1>
 <h2>Asia ArmpitandAssSmells</h2>
 <button>Asia StinkSmells</button>
